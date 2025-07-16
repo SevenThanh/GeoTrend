e67a89c8c84a45f203b09a11356c0413c2badda8
Johan Nguyen <thaithanhace@gmail.com>
2025-07-16 09:52:22 -0500

    added map page

---
+++ GeoTrend/src/pages/MapPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapComponent from '../components/MapComponent';
+
+const MapPage = () => {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Interactive World Map</h1>
+      </div>
+    );
+};
+export default MapPage;
+
+  

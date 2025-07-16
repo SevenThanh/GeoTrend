be911b56a88b6fbba54bc93d23e539a22fc90b0d
Johan Nguyen <thaithanhace@gmail.com>
2025-07-15 22:10:41 -0500

    set up folders structure, download frameworks

---
+++ GeoTrend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-white text-black flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Basic Render Test</h1>
+    </div>
+  );
+}
+
+export default App;
